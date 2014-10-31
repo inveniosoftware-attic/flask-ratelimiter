a8bd82341d931b2aa49240807fc95d59ba6e556b
Jiri Kuncar <jiri.kuncar@cern.ch>
2014-10-31 15:51:20 +0100

    limiter: optionally callable limit argument
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- flask_ratelimiter/__init__.py
+++ flask_ratelimiter/__init__.py
@@ -7,9 +7,7 @@
 # modify it under the terms of the Revised BSD License; see LICENSE
 # file for more details.
 
-"""
-Flask extension
-===============
+"""Extension for Flask that adds support for request rate limiting.
 
 Flask-RateLimiter is initialized like this:
 
@@ -30,12 +28,8 @@ You can use ratelimit decorator like this:
     >>> def some_view():
     >>>     return 'HelloWorld'
 
-Based on:
-
-* Flask-ext-skeleton
-
-* Snippet of code by Armin Ronacher from:
-  http://flask.pocoo.org/snippets/70/
+It is based on snippet of code by Armin Ronacher from
+`http://flask.pocoo.org/snippets/70/`_.
 """
 
 from __future__ import absolute_import
@@ -44,10 +38,14 @@ from flask import current_app, request, g
 from functools import update_wrapper
 from six import iteritems
 
+# Version information
+from .version import __version__
+
 
 class RateLimitInfo(object):
-    """
-    Stores information about rate limiting.
+
+    """Store information about rate limiting.
+
     Can be saved in 'g' object for further use.
     """
 
@@ -58,19 +56,19 @@ class RateLimitInfo(object):
 
 
 def on_over_limit(rate_limit_info):
-    """
-    Default response which will be displayed
-    when rate limit is exceeded.
+    """Default response which will be displayed when rate limit is exceeded.
+
     :param rate_limit_info: RateLimitInfo object
     """
     return "Rate limit was exceeded", 429
 
 
 class RateLimiter(object):
-    """
-    Flask extension
 
-    Initialization of the extension:
+    """
+    Implement RateLimiter extension.
+
+    Initialization of the extension can be done via following code:
 
     .. code-block:: pycon
 
@@ -94,10 +92,7 @@ class RateLimiter(object):
             self.init_app(app)
 
     def init_app(self, app):
-        """
-        Initialize a Flask extension.
-        """
-
+        """Initialize a Flask extension."""
         if not hasattr(app, 'extensions'):
             app.extensions = {}
         if 'ratelimiter' in app.extensions:
@@ -129,10 +124,9 @@ class RateLimiter(object):
 
     @staticmethod
     def get_backend(name):
-        """
-        Returns backend for RateLimiter.
-        If there is no such backend, default backend
-        will be returned.
+        """Return backend for RateLimiter.
+
+        If there is no such backend, default backend will be returned.
         """
         from . import backends
         try:
@@ -142,9 +136,7 @@ class RateLimiter(object):
         return backend
 
     def _change_prefix_if_flask_cache(self, config):
-        """
-        Uses Flask-Cache prefix by default if FlaskCache backend is used.
-        """
+        """Use Flask-Cache prefix by default if FlaskCache backend is used."""
         if config.get('CACHE_KEY_PREFIX', '') and \
            config.get('RATELIMITER_BACKEND').startswith('FlaskCache'):
             config['RATELIMITER_KEY_PREFIX'] = config['CACHE_KEY_PREFIX']
@@ -154,22 +146,31 @@ def ratelimit(limit, per=300, send_x_headers=True,
               over_limit=on_over_limit,
               scope_func=lambda: request.remote_addr,
               key_func=lambda: request.endpoint):
-    """
-    Decorator can be used for rate limiting.
+    """Decorator can be used for rate limiting.
 
     Example:
 
-    @ratelimit(30, 10)
-    def index():
-        return "HelloWorld"
+    .. code-block:: python
+
+        @ratelimit(30, 10)
+        def index():
+            return "HelloWorld"
+
+    :param limit: int or callable accepting keyword arguments key and scope.
     """
     def decorator(f):
         def rate_limited(*args, **kwargs):
             ratelimiter = current_app.extensions['ratelimiter']
             prefix = current_app.config['RATELIMITER_KEY_PREFIX']
-            key = prefix + '/%s/%s/' % (key_func(), scope_func())
+            key = key_func()
+            scope = scope_func()
+            current_limit = limit(key=key, scope=scope) if callable(limit) \
+                else limit
+
             limit_exceeded, remaining, reset = ratelimiter.backend.update(
-                key, limit, per)
+                '{prefix}/{key}/{scope}'.format(prefix=prefix, key=key,
+                                                scope=scope),
+                current_limit, per)
 
             info = RateLimitInfo(limit=limit,
                                  per=per,
@@ -184,10 +185,6 @@ def ratelimit(limit, per=300, send_x_headers=True,
             return f(*args, **kwargs)
         return update_wrapper(rate_limited, f)
     return decorator
-
-
-# Version information
-from .version import __version__
 
 __all__ = [
     'RateLimiter', '__version__', 'ratelimit'
